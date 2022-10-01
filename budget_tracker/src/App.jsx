11a3bc5d8e02d4aaff31af8bbdc21daf17e60d9b
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-10-02 00:11:12 +0530

    initial commit budget tracker

---
+++ budget_tracker/src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Container from 'react-bootstrap/Container';
+import { Button, Stack } from 'react-bootstrap';
+import BudgetCard from "./components/BudgetCard.component";
+import AddBudgetModel from "./components/AddBudgetModel";
+import AddExpenseModel from "./components/AddExpenseModel";
+import { useBudgets } from "./contexts/BudgetContext";
+import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard";
+import TotalBudgetCard from "./components/TotalBudgetCard";
+
+
+function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  const [showAddExpenseModal, setShowAddExpenseModal] = useState(false);
+  const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState();
+  const { budgets, getBudgetExpenses } = useBudgets();
+
+  function openAddExpenseModal(budgetId){
+    setShowAddExpenseModal(true);
+    setAddExpenseModalBudgetId(budgetId)
+  }
+
+  return (
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
+          <Button variant="outline-primary" onClick={ openAddExpenseModal }>Add Expense</Button>
+        </Stack>
+        <div style={{display:"grid", gridTemplateColumns:"repeat(auto-fill, minmax(300px, 1fr))", gap:"1rem", alignItems:"flex-start"}}>
+          {budgets.map(budget => {
+            const amount = getBudgetExpenses(budget.id).reduce((total, expense) => total + expense.amount, 0)
+
+            return (
+              <BudgetCard key={budget.id} name={budget.name} amount={amount} max={budget.max} onAddExpenseClick={() => openAddExpenseModal(budget.id)}/>
+            )
+          })}
+          <UncategorizedBudgetCard/>
+          <TotalBudgetCard/>
+        </div>
+      </Container>
+      <AddBudgetModel show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)}/>
+      <AddExpenseModel show={showAddExpenseModal} defaultBudgetId={addExpenseModalBudgetId} handleClose={() => setShowAddExpenseModal(false)}/>
+    </>
+  );
+}
+
+export default App;
---
+++ budget_tracker/src/contexts/BudgetContext.jsx
@@ -0,0 +1,61 @@
+import React, { useContext } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const BudgetsContext = React.createContext();
+
+export const UNCATEGORIZED_BUDGET_ID = "Uncategorized"
+
+export function useBudgets(){
+    return useContext(BudgetsContext);
+}
+
+export const BudgetsProvider = ({ children }) => {
+
+    const [budgets, setBudgets] = useLocalStorage( "budgets", []);
+    const [expenses, setExpenses] = useLocalStorage( "expenses", []);
+
+    function getBudgetExpenses(budgetId){
+        return expenses.filter(expense => expense.budgetId === budgetId)
+    }
+
+    function addExpenses({ description, amount, budgetId }){
+        setExpenses(prevExpenses => {
+            return [...prevExpenses, { id: uuidv4(), description, amount, budgetId }]
+        })
+    }
+
+    function addBudget({ name, max }){
+        setBudgets(prevBudgets => {
+            if(prevBudgets.find(budget => budget.name === name)){
+                return prevBudgets
+            };
+            return [...prevBudgets, { id: uuidv4(), name, max }]
+        })
+    }
+
+    function deleteBudget({ id }){
+        setBudgets(prevBudgets => {
+            return prevBudgets.filter(budget => budget.id !== id)
+        })
+    };
+    
+    function deleteExpense({ id }){
+        setExpenses(prevExpenses => {
+            return prevExpenses.filter(expense => expense.id !== id)
+        })
+    };
+
+
+    return <BudgetsContext.Provider value={{
+        budgets,
+        expenses,
+        getBudgetExpenses,
+        addExpenses,
+        addBudget,
+        deleteBudget,
+        deleteExpense
+    }}>
+        {children}
+    </BudgetsContext.Provider>
+};
